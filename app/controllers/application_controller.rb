c59045f9f4e8629199ccb954138659c75a3dd87f
William Price <price.james.william@gmail.com>
2023-04-18 05:25:19 +0000

    added soultions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   def blank_square_form
     render({ :template => "calculation_templates/square_form.html.erb" })
   end
+
+  def calculate_square
+    @num = params.fetch("blue").to_f
+    @square_or_num = @num ** 2
+
+    render({ :template => "calculation_templates/square_results.html.erb" })
+  end
 end
