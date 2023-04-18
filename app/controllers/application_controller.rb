fed6c0fee103a4b91d9b9d03112c7ec6cf09626d
William Price <price.james.william@gmail.com>
2023-04-18 08:08:41 +0000

    added soultions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,38 @@ class ApplicationController < ActionController::Base
 
   def calculate_square
     @num = params.fetch("blue").to_f
-    @square_or_num = @num ** 2
+    @square_or_num = @num * @num
 
     render({ :template => "calculation_templates/square_results.html.erb" })
+  end
+
+  def blank_square_root_form
+    render({ :template => "calculation_templates/square_root_form.html.erb" })
+  end
+
+  def calculate_square_root
+    @num = params[:blue].to_f
+    @square_root_of_num = Math.sqrt(@num)
+
+    render(template: "calculation_templates/square_root_reults.html.erb")
+  end
+
+  def payment_form
+    render({ :template => "calculation_templates/payment_form.html.erb" })
+  end
+
+  def calculate_payment
+    @apr = params[:apr].to_f / 100 / 12
+    @number_of_payments = params[:number_of_years].to_i * 12
+    @principal = params[:principal].to_f
+
+    @monthly_payment = (@principal * @apr * (1 + @apr) ** @number_of_payments) / ((1 + @apr) ** @number_of_payments - 1)
+
+    @apr = params[:apr]
+    @number_of_years = params[:number_of_years]
+    @principal = params[:principal]
+    @monthly_payment = "%.2f" % @monthly_payment
+
+    render({ :template => "calculation_templates/payment_results.html.erb" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   get("/square/new", { :controller => "application", :action => "blank_square_form" })
 
   get("/square/results", { :controller => "application", :action => "calculate_square" })
+
+  get("/square_root/new", { :controller => "application", :action => "blank_square_root_form" })
+
+  get("/square_root/results", { :controller => "application", :action => "calculate_square_root" })
+
+  get("/payment/new", { :controller => "application", :action => "payment_form" })
+
+  get("/calculate_payment", { :controller => "application", :action => "calculate_payment" })
 end
-
-
-
